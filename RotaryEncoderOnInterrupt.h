23f4bd2f9c0baa68ecba7a7279c07b06a67b052f
Natan Lisowski <82398767+natnqweb@users.noreply.github.com>
2024-10-01 20:08:14 +0200

    Update RotaryEncoderOnInterrupt.h

--- RotaryEncoderOnInterrupt.h
+++ RotaryEncoderOnInterrupt.h
@@ -2,9 +2,9 @@
 
 static void _f_s_EmptyFunc_(){}
 
-#define CREATE_ENCODER(NAME, PIN_A, PIN_B) \
-static rotary_encoder NAME{PIN_A, PIN_B}; \
-static void RisingFunc_##NAME() { \
+#define REOI_CREATE_ENCODER(NAME, PIN_A, PIN_B) \
+static t_rotary_encoder NAME{PIN_A, PIN_B}; \
+static void REOI_RisingFunc_##NAME() { \
   if (digitalRead(NAME._a)) { \
     NAME.pulses++; \
   } else { \
@@ -12,27 +12,27 @@ static void RisingFunc_##NAME() { \
   } \
 }
 
-#define GET_ENCODER_RISING_ROUTINE(NAME) \
-RisingFunc_##NAME
+#define REOI_GET_ENCODER_RISING_ROUTINE(NAME) \
+REOI_RisingFunc_##NAME
 
-#define INIT_INTERRUPTS(NAME) \
+#define REOI_INIT_INTERRUPTS(NAME) \
 attachInterrupt(digitalPinToInterrupt(NAME._b), NAME.fnc, RISING)
 
-#define INIT_ENCODER(NAME) \
+#define REOI_INIT_ENCODER(NAME) \
 pinMode(NAME._a, INPUT_PULLUP); \
 pinMode(NAME._b, INPUT_PULLUP); \
-NAME.fnc = GET_ENCODER_RISING_ROUTINE(NAME); \
-INIT_INTERRUPTS(NAME)
+NAME.fnc = REOI_GET_ENCODER_RISING_ROUTINE(NAME); \
+REOI_INIT_INTERRUPTS(NAME)
 
 
-#define GET_PULSES(NAME) \
+#define REOI_GET_PULSES(NAME) \
 NAME.pulses
 
-using rising_f = void(*)();
-class rotary_encoder {
+using f_rising = void(*)();
+class t_rotary_encoder {
   public:
     int _a, _b;
-    explicit rotary_encoder(int a, int b) : _a{a}, _b{b} {};
+    explicit t_rotary_encoder(int a, int b) : _a{a}, _b{b} {};
     volatile long pulses{0};
-    rising_f fnc{_f_s_EmptyFunc_};
+    f_rising fnc{_f_s_EmptyFunc_};
 };
